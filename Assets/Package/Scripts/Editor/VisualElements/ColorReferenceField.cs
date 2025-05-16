91746973c891e44c94333d8f07fec600bc04cba7
Leniton <leniton.carneiro@gdstecnologia.com.br>
2025-05-16 15:24:51 -0300

    feat: color reference style

--- Assets/Package/Scripts/Editor/VisualElements/ColorReferenceField.cs
+++ Assets/Package/Scripts/Editor/VisualElements/ColorReferenceField.cs
@@ -8,6 +8,7 @@ using UnityEngine.UIElements;
 public class ColorReferenceField : BaseField<Color>
 {
     private ColorField colorField;
+    private Label countLabel => colorField.labelElement;
 
     private int count;
 
@@ -41,8 +42,11 @@ public class ColorReferenceField : BaseField<Color>
         colorField.style.maxWidth = 220;
         colorField.style.alignItems = Align.FlexEnd;
         colorField.style.flexGrow = 1;
-        colorField.labelElement.style.minWidth = 20;
-        colorField.labelElement.style.unityTextAlign = TextAnchor.MiddleRight;
+        countLabel.SetBorder(1.5f, 2f, Color.black);
+        countLabel.style.backgroundColor = ColorExtension.GrayShade(.2f);
+        countLabel.style.width = 20;
+        countLabel.style.minWidth = 20;
+        countLabel.style.unityTextAlign = TextAnchor.LowerCenter;
         contentContainer.Add(colorField);
     }
 }
